1f6fbc9a8bdbb4336ddb6879453a214fe2bc5e41
maryo222 <maryanneorton@gmail.com>
2017-03-20 14:56:54 +1300

    Added add project function, gets info to the database

--- includes/functions.php
+++ includes/functions.php
@@ -20,3 +20,23 @@ function getProject($dbh) {
 	$result = $sth->fetchAll();
 	return $result;
 }
+
+
+
+// this function adds the content of thr feedback form to database
+function addProject($dbh, $title, $image_url, $content, $link) {
+	// prepare statement that will be executed
+	$sth = $dbh->prepare("INSERT INTO projects (title, image_url, content, link, created_at, updated_at) VALUES (:title, :image_url, :content, :link, NOW(), NOW())");
+	// bind the $name to the SQL statement
+	$sth->bindParam(':title', $title, PDO::PARAM_STR);
+	// bind the $email to the SQL statement
+	$sth->bindParam(':image_url', $image_url, PDO::PARAM_STR);
+	// bind the $feedback to the SQL statement
+	$sth->bindParam(':content', $content, PDO::PARAM_STR);
+		// bind the $feedback to the SQL statement
+	$sth->bindParam(':link', $link, PDO::PARAM_STR);
+	// execute the statement 
+	$success = $sth->execute();
+
+	return $success;
+}
