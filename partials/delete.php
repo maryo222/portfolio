092a994ed3bf823e3ed820c1ec9c920c0a4c5a07
maryo222 <maryanneorton@gmail.com>
2017-04-02 19:34:00 +1200

    Delete page didn't need

--- partials/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require 'includes/config.php';
-	$id=$_POST['id'];
-	$result = $db->prepare("DELETE FROM projects WHERE id= :id");
-	$result->bindParam(':id', $id);
-	$result->execute();
-	header("location: index.php");
