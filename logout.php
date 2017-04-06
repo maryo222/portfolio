6f126294af1da81afdfb63000083fc44b8b21ad4
maryo222 <maryanneorton@gmail.com>
2017-04-06 14:21:55 +1200

    Logout page

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+require 'includes/config.php';
+
+// destroy the session and redirect
+session_destroy();
+session_start();
+
+addMessage(('success'), "You have been logged out");
+redirect('index.php');
