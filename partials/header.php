26f1e1e808bfcc7b8c98dbea249985791f22ee70
maryo222 <maryanneorton@gmail.com>
2017-04-10 08:46:15 +1200

    Added extra styling

--- partials/header.php
+++ partials/header.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Portfolio</title>
-
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Styles -->
     <!-- Bootstrap -->
     <link href="css/app.css" rel="stylesheet">
