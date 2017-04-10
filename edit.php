23cf887df2a2d266dc1d1553cb46c621b4c21d1b
maryo222 <maryanneorton@gmail.com>
2017-04-10 16:59:17 +1200

    Reformatted edit page

--- edit.php
+++ edit.php
@@ -13,63 +13,94 @@
 
 ?>
 
-	<form class="form-horizontal" role="form" method="POST" action="edit.php">
-		<input name="id" value="<?= $editProject['id'] ?>" type="hidden" >
 
-	    <!-- Form Title -->
-	    <div class="form-group">
-	        <label class="col-md-4">
-	            Edit project
-	        </label>
-	    </div>
+<!-- Start of Content -->
+<div class="container">
 
-	    <!-- Title Input -->
-	    <div class="form-group">
-	        <label for="projectName" class="col-md-4 control-label">Title</label>
+  <div class="row">
+    <div class="col-md-12">
+    </div>
+  </div>
 
-	        <div class="col-md-6">
-	            <input id="projectName" type="text" class="form-control" name="title" value="<?=$editProject['title']?>" required="" autofocus="">
-	        </div>
-	    </div>
+  <div class="row">
+    <div class="col-md-8">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          Edit
+        </div>
+        <div class="panel-body">
+          <form class="form-horizontal" role="form" method="POST" action="edit.php">
+            <input id="id" type="hidden" name="id" value="<?= $editProject['id'] ?>">
 
-	    <!-- Image Url Input -->
-	    <div class="form-group">
-	        <label for="projectImgUrl" class="col-md-4 control-label">Image Url</label>
 
-	        <div class="col-md-6">
-	            <input id="projectImgUrl" type="text" class="form-control" name="image_url" value="<?=$editProject['image_url']?>" required="" autofocus=""  onchange="readURL(this)">
-	        </div>
-	    </div>
 
-	    <!-- Content Input -->
-	    <div class="form-group">
-	        <label for="projectContent" class="col-md-4 control-label">Content</label>
+            <!-- Form Title -->
+            <div class="form-group">
+              <label class="col-md-4">
+                Edit project
+              </label>
+            </div>
 
-	        <div class="col-md-6">
-	            <input id="projectContent" type="text" class="form-control" name="content" value="<?=$editProject['content']?>" required="" autofocus="">
-	        </div>
-	    </div>
+            <!-- Title Input -->
+            <div class="form-group">
+              <label for="projectName" class="col-md-4 control-label">Title</label>
 
-	    <!-- Link Input -->
-	    <div class="form-group">
-	        <label for="projectLink" class="col-md-4 control-label">Link</label>
+              <div class="col-md-6">
+                <input id="title" type="text" class="form-control" name="title" value="<?= $editProject['title'] ?>" required="" autofocus="">
+              </div>
+            </div>
 
-	        <div class="col-md-6">
-	            <input id="projectLink" type="text" class="form-control" name="link" value="<?=$editProject['link']?>" required="" autofocus="">
-	        </div>
-	    </div>
+            <!-- Image Url Input -->
+            <div class="form-group">
+              <label for="image_url" class="col-md-4 control-label">Image Url</label>
 
-	     <!-- Submit Button -->
-	    <div class="form-group">
-	        <div class="col-md-8 col-md-offset-4">
-	            <button type="submit" class="btn btn-primary">
-	                Update
-	            </button>
-	        </div>
-	    </div>
+              <div class="col-md-6">
+                <input id="image_url" type="text" class="form-control" name="url" value="<?= $editProject['image_url'] ?>" required="" autofocus="" onchange="readURL(this)">
+              </div>
+            </div>
 
-	</form>
+            <!-- Content Input -->
+            <div class="form-group">
+              <label for="projectContent" class="col-md-4 control-label">Content</label>
 
+              <div class="col-md-6">
+                <input id="content" type="text" class="form-control" name="content" value="<?= $editProject['content'] ?>" required="" autofocus="">
+              </div>
+            </div>
+
+            <!-- Link Input -->
+            <div class="form-group">
+              <label for="projectLink" class="col-md-4 control-label">Link</label>
+
+              <div class="col-md-6">
+                <input id="link" type="text" class="form-control" name="link" value="<?= $editProject['link'] ?>" required="" autofocus="">
+              </div>
+            </div>
+
+            <!-- Submit Button -->
+            <div class="form-group">
+              <div class="col-md-8 col-md-offset-4">
+                <button type="submit" class="btn btn-primary">
+                  Update
+                </button>
+              </div>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <!-- Image Display Thumbnail -->
+      <div class="form-group">
+        <img style="display: block;" width="100%" height="100%" id="projectThumbnail" src="<?= $editProject['image_url'] ?>" class="img-responsive">
+      </div>
+    </div>
+  </div>
+</div>
+
+	
 <?php 
     require 'partials/footer.php';
 ?>
